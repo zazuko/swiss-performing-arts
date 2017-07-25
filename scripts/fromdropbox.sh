a39366431ae77584ea5e34b2706980b0b6b36ad8
Adrian Gschwend <ktk@netlabs.org>
2017-07-25 16:40:22 +0200

    Import files from Dropbox folder

---
+++ scripts/fromdropbox.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+source scripts/env.sh
+cp $DROPBOX/20170704_Update/Entwuerfe*.csv ./input/STS/Entwuerfe.csv
+cp $DROPBOX/20170704_Update/Masken*.csv ./input/STS/Masken.csv
+cp $DROPBOX/20170704_Update/Modelle*.csv ./input/STS/Modelle.csv
+cp $DROPBOX/20170704_Update/Plakate*.csv ./input/STS/Plakate.csv
+cp $DROPBOX/20170704_Update/RepProfi*.csv ./input/STS/RepProfi.csv
+# datahub
+cp $DROPBOX/20170725_Update_datahubio/stc_amateur_theatre_companies_*.csv ./input/STS/stc_amateur_theatre_companies.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_anniversary_performing_arts_festivals_*.csv ./input/STS/stc_anniversary_performing_arts_festivals.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_circuses_*.csv ./input/STS/stc_circuses.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_outdoor_theatre_events_*.csv ./input/STS/stc_outdoor_theatre_events.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_performing_arts_festivals_*.csv ./input/STS/stc_performing_arts_festivals.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_professional_performing_arts_companies_*.csv ./input/STS/stc_professional_performing_arts_companies.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_puppet_theatre_companies_*.csv ./input/STS/stc_puppet_theatre_companies.csv
+cp $DROPBOX/20170725_Update_datahubio/stc_revues_cultural_nights_vorfasnacht_*.csv ./input/STS/stc_revues_cultural_nights_vorfasnacht.csv
