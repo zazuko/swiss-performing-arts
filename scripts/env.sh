0c60144eb1ab321d521a79a4cf4aecadecb5323e
Adrian Gschwend <ktk@netlabs.org>
2017-08-21 23:44:50 +0200

    Adding schema

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,5 @@
 FUSEKI_HOST=localhost
 SCOPE2RDF=~/workspace/zazuko/scope-to-rdf
 DROPBOX=~/Dropbox/AP2_3_Datenmodell_und–migration/Daten_STS
+DROPBOXBASE=~/Dropbox/AP2_3_Datenmodell_und–migration
 TANZARCHIV=~/Dropbox/AP2_3_Datenmodell_und–migration/Daten_STA/20161117_xmlexport_scope_sta
