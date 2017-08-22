b43262c2b9d38d9387b120ec13c10583a1a8c8c1
Adrian Gschwend <ktk@netlabs.org>
2017-08-22 09:43:18 +0200

    Fixed link to schema

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source scripts/env.sh
-./node_modules/xlsx/bin/xlsx.njs $DROPBOXBASE/SPA_Data_Model_Attributes_20170523.xlsx -o input/SPA_Attributes.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOXBASE/SPA_Data_Model_Classes_export_20170501.xlsx -o input/SPA_Classes.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOXBASE/SPA_Data_Model_Qualifiers_20170523.xlsx -o input/SPA_Qualifiers.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOXBASE/SPA_Data_Model_Relations_20170620.xlsx -o input/SPA_Relations.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/../SPA_Data_Model_Attributes_20170523.xlsx -o input/SPA_Attributes.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/../SPA_Data_Model_Classes_export_20170501.xlsx -o input/SPA_Classes.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/../SPA_Data_Model_Qualifiers_20170523.xlsx -o input/SPA_Qualifiers.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/../SPA_Data_Model_Relations_20170620.xlsx -o input/SPA_Relations.csv
