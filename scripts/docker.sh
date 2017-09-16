1e8b3622a293dfd9a45e3ec3e031e91be192af09
Adrian Gschwend <ktk@netlabs.org>
2017-09-16 14:12:55 +0200

    Adjust Fuseki memory limit

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,1 +1,1 @@
-docker run --rm --name fuseki-spa -p 3030:3030 -e ADMIN_PASSWORD=cinderella stain/jena-fuseki
+docker run --rm --name fuseki-spa -p 3030:3030 -e ADMIN_PASSWORD=cinderella -e JVM_ARGS=-Xmx2g stain/jena-fuseki
--- scripts/lindas-put.sh
+++ scripts/lindas-put.sh
@@ -11,3 +11,12 @@ curl -n \
      -T target/STA/complete.nt \
      -G https://test.lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/SPA/tanzarchiv
+curl -n \
+     -X PUT \
+     -H Content-Type:application/n-triples \
+     -T target/schema.nt \
+     -G https://test.lindas-data.ch:8443/lindas \
+     --data-urlencode graph=https://linked.opendata.swiss/graph/SPA/schema
+
+
+     
