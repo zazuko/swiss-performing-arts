82658e83c59f3faf81bb563503c943119d1a5155
Adrian Gschwend <ktk@netlabs.org>
2018-02-19 19:06:16 +0100

    Switch to -current

--- scripts/fromdropboxschema.sh
+++ scripts/fromdropboxschema.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source scripts/env.sh
-./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Attributes_20170904.xlsx -o input/SPA_Attributes.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Classes_export_20170828.xlsx -o input/SPA_Classes.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Qualifiers_20170904.xlsx -o input/SPA_Qualifiers.csv
-./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Relations_20170904.xlsx -o input/SPA_Relations.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Attributes_current.xlsx -o input/SPA_Attributes.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Classes_current.xlsx -o input/SPA_Classes.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Qualifiers_current.xlsx -o input/SPA_Qualifiers.csv
+./node_modules/xlsx/bin/xlsx.njs $DROPBOX/SPA_Data_Model_Relations_current.xlsx -o input/SPA_Relations.csv
